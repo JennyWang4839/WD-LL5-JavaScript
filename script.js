c7835f83b2bfa877633aaacb87b3471d44676bf2
Jenny Wang <142929110+JennyWang4839@users.noreply.github.com>
2026-02-18 00:07:17 +0000

    Finished all tasks

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Hello, World!");
 
+console.error("This is my first JavaScript error!");
+
+alert("Jenny Wang"); 
+
+alert("Jaskirat Singh"); 
+
+alert("Dane Alonzo"); 
+
+alert("Mariam Coulibaly");
+
+alert("James Verhoeven");
+
+console.log(console);
